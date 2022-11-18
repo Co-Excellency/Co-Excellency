5e739dcac1977037ec6d56ec0a6a3ef19fb81cc0
lcw3176 <lcw3176@naver.com>
2022-11-18 15:14:30 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <div class="container pt-5">
+      <SmallNavbar />
+      <Navbar />
+    </div>
+
+
+    <div class="container has-background-light">
+      <Chart />
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+import SmallNavbar from './components/SmallNavbar.vue'
+import Chart from './components/Chart.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    SmallNavbar,
+    Chart
+  },
+}
+</script>
---
+++ src/components/SmallNavbar.vue
@@ -0,0 +1,18 @@
+<template>
+	<section>
+        <b-breadcrumb
+            align="is-right"
+            separator="has-bullet-separator"
+            size="is-small"
+        >
+            <b-breadcrumb-item href="/">로그인</b-breadcrumb-item>
+            <b-breadcrumb-item href="/">회원가입</b-breadcrumb-item>
+        </b-breadcrumb>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'SmallNavbar',
+}
+</script>
